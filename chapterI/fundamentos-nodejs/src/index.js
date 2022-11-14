62dd616c86837d0924db899219934462358240ca
Alexandre Washington Moreira de Souza <a.moreira.de.souza@avanade.com>
2022-11-14 16:10:21 -0300

    Retornando um json

--- chapterI/fundamentos-nodejs/src/index.js
+++ chapterI/fundamentos-nodejs/src/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.send("Hello world!")
+    return response.json({message: "Hello world!"})
 })
 app.listen(3333);

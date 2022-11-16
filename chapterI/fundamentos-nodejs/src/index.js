afcd0bc50bebc904dda1edf7d0d25e72ac5dcf04
Alexandre Washington Moreira de Souza <a.moreira.de.souza@avanade.com>
2022-11-16 09:29:23 -0300

    Utilizando metodos http

--- chapterI/fundamentos-nodejs/src/index.js
+++ chapterI/fundamentos-nodejs/src/index.js
@@ -2,8 +2,32 @@ const { response } = require('express');
 const express = require('express');
 
 const app = express();
+/**
+    * GET  - Buscar uma informação dentro do servidor
+    * POST - Inserir uma informação no servidor
+    * PUT  - Alterar uma informação no servidor
+    * PATCH - Alterar uma informação específica no servidor
+    * DELETE - Deletar uma informação no servidor
+*/
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello world!"})
+app.get("/courses", (request, response) => {
+    return response.json(["Front End", "Back End", "Mobile"])
 })
+
+app.post("/courses", (request, response) => {
+    return response.json(["Front End", "Back End", "Mobile", "Scrum Master"])
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json(["Front End X", "Back End", "Mobile", "Scrum Master"])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Front End X", "Back End Z", "Mobile", "Scrum Master"])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Front End X", "Back End Z", "Scrum Master"])
+})
+
 app.listen(3333);

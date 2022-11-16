33bae3997672f53f610bbf3e575fbde94c90372f
Alexandre Washington Moreira de Souza <a.moreira.de.souza@avanade.com>
2022-11-16 10:40:38 -0300

    Tipos de parametros

--- chapterI/fundamentos-nodejs/src/index.js
+++ chapterI/fundamentos-nodejs/src/index.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 /**
     * GET  - Buscar uma informação dentro do servidor
     * POST - Inserir uma informação no servidor
@@ -10,15 +11,29 @@ const app = express();
     * DELETE - Deletar uma informação no servidor
 */
 
+/*
+    * Tipos de parametro
+    *
+    * Route Params => Identificar um recurso editar|deletar|busacar
+    * Query Params => Paginacao | Filtro
+    * Body Params  => Os objetos inserção | alteração (JSON)
+*/
+
 app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query)
     return response.json(["Front End", "Back End", "Mobile"])
 })
 
 app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body)
     return response.json(["Front End", "Back End", "Mobile", "Scrum Master"])
 })
 
 app.put("/courses/:id", (request, response) => {
+    const {id} = request.params;
+    console.log(id)
     return response.json(["Front End X", "Back End", "Mobile", "Scrum Master"])
 })
 
---
+++ chapterI/fundamentos-nodejs/src/notes.md
@@ -0,0 +1,11 @@
+# Fudamentos NodeJs
+    [] Introdução
+    [] Conceitos
+    [] Conceitos sobre API Rest
+    [] Métodos de Requisição
+    [] Criando estrutura do projeto
+    [] Adicionando Nodemon na Aplicação
+    [] Utilizando os métodos HTTP
+    [] Utilizando o Insomnia
+    [] Tipos de parâmetros
+    [] Repositório CapI - https://github.com/alexandre-moreira/ignite-nodejs/tree/main/chapterI

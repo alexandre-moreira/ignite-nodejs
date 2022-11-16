4e2450f401c581095b7d4f4a68aade9683b04eb0
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-11-16 18:59:06 -0300

    Criando deposito na conta

--- chapterI/finapi/src/index.js
+++ chapterI/finapi/src/index.js
@@ -14,7 +14,20 @@ const customers = [];
  * id - uuid
  * statement
  */
+function verifyIfExistsAccountCPF(request, response, next){
+    const {cpf} = request.headers;
 
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer){
+        return response.status(400).json({error: "Customer not found"})
+    }
+
+    request.customer = customer;
+
+    return next();
+
+}
 app.post("/account", (request, response) => {
     const {cpf, name } = request.body;
 
@@ -38,16 +51,26 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 })
 
-app.get("/statement",(request, response) =>{
-    const {cpf} = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer){
-        return response.status(400).json({error: "Customer not found"})
-    }
-
+app.get("/statement",verifyIfExistsAccountCPF,(request, response) =>{
+    const {customer} = request;
     return response.json(customer.statement);
 });
+
+app.post("/deposit", verifyIfExistsAccountCPF,(request, response) =>{
+    const {description, amount} = request.body;
+
+    const {customer} = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
 
 app.listen(3333);

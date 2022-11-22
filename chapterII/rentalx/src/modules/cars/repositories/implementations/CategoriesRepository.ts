a67ee24322fd7515db80680820b04b449cbf075f
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-11-22 10:01:09 -0300

    Refatorando rotas

--- chapterII/rentalx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ chapterII/rentalx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,16 +1,25 @@
-import { Category } from "../model/Category";
+import { Category } from "../../model/Category";
 import {
     ICategoriesRepository,
     ICreateCategoryDTO,
-} from "./ICategoriesRepository";
+} from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
+
+    private static INSTANCE: CategoriesRespository;
 
     constructor() {
         this.categories = [];
     }
 
+    public static getIntance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+
+        return CategoriesRepository.INSTANCE;
+    }
     create({ description, name }: ICreateCategoryDTO): void {
         const category = new Category();
 
